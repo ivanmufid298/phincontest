74b37da9726c7d3e048c87d0d0cf0799dcafe3de
ivan mufid <ivanmohammad2355@gmail.com>
2023-03-07 15:54:18 +0700

    Qualification 50%

--- phincon-test/src/components/header/Header.jsx
+++ phincon-test/src/components/header/Header.jsx
@@ -40,7 +40,7 @@ const Header = () => {
 
                     <li className="nav__item">
                         <a href="#portfolio" className="nav__link">
-                            <i className="uil uil-scenery nav__icon"></i> Portfolio
+                            <i className="uil uil-scenery nav__icon"></i> Blog
                         </a>
                     </li>
 
---
+++ phincon-test/src/components/qualification/Qualification.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import "./qualification.css"
+/* eslint-disable */
+
+const Qualification = () => {
+  return (
+    <section className="qualification section">
+        <h2 className="section__title">Qualification</h2>
+        <span className="section__subtitle">My Personal Journey</span>
+
+        <div className="qualification__container container">
+            <div className="qualification__tabs">
+                <div className="qualification__button qualivication_active button--flex">
+                    <i className="uil uil-graduation-cap qualification__icon"></i>Education
+                </div>
+
+                <div className="qualification__button button--flex">
+                    <i className="uil uil-briefcase-alt qualification__icon"></i>Experience
+                </div>
+            </div>
+
+            <div className="qualification__sections">
+                <div className="qualification__content qualification__content-active">
+                    <div className="qualification__data">
+                    <div>
+                        <h3 className="qualification__title">Web Design</h3>
+                        <span className="qualification__subtitle">India - Institute</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i>2021 - Present
+                        </div>
+                    </div>
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification__title">Art Design</h3>
+                        <span className="qualification__subtitle">India - Institute</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i>2021 - Present
+                        </div>
+                      </div>
+                   </div>
+
+                   <div className="qualification__data">
+                    <div>
+                        <h3 className="qualification__title">Web Development</h3>
+                        <span className="qualification__subtitle">India - Institute</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i>2021 - Present
+                        </div>
+                    </div>
+                    <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+                    </div>
+
+                    <div className="qualification__data">
+                        <div></div>
+
+                        <div>
+                        <span className="qualification__rounder"></span>
+                        <span className="qualification__line"></span>
+                    </div>
+
+                    <div>
+                        <h3 className="qualification__title">UX Expert</h3>
+                        <span className="qualification__subtitle">India - Institute</span>
+                        <div className="qualification__calender">
+                            <i className="uil uil-calendar-alt"></i>2021 - Present
+                        </div>
+                      </div>
+                   </div>
+                </div>   
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Qualification
