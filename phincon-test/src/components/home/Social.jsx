79e2b909ede65514b2d57618a50d1b8bce8d29b2
ivan mufid <ivanmohammad2355@gmail.com>
2023-03-07 10:20:36 +0700

    Adding Profile Data

---
+++ phincon-test/src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+/* eslint-disable */ 
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="" className="home__social-icon" target="_blank">
+            <i class="uil uil-instagram-alt"></i>
+        </a>
+
+        <a href="" className="home__social-icon" target="_blank">
+            <i class="uil uil-dribbble"></i>
+        </a>
+
+        <a href="" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
